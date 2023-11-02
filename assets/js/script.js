6e20b92ee30e3c48039dd3c0cb65abcb06cb5989
jyoon3 <jyoon3@2u.com>
2023-11-02 17:16:23 -0400

    simplified the logic to make it more digestible, added all to single table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
 console.log("AYAYA");
-// I want to add functionality wherein 
+// I want to add functionality wherein when I click the button, it should DISPLAY the price, origin, and weight of the fish.
+
+// I want to make ONE CLICK EVENT CAUSE IM LAZY. SO I AM GOING TO TARGET THE PARENT ELEMENT
+
+var fishCardBody = document.getElementById('fishBody');
+
+console.log(fishCardBody);
+
+// When I click on a card inside of this fishBody, I want to get the origin. But let's start by confirming that I can click a specific card in this body.
+
+fishCardBody.addEventListener('click', function(e) {
+    console.log(e.target);
+    var buttonOnCard = e.target;
+
+    if(buttonOnCard.matches("button")) {
+        // console.log("You clicked a button!");
+        // console.log(e.target);
+        var dataOfOrigin = buttonOnCard.getAttribute('data-origin');
+        console.log(dataOfOrigin);
+
+        var listEl = document.createElement('li');
+        listEl.textContent = dataOfOrigin;
+       var mackerelTable = document.getElementById('mackerel-table');
+       mackerelTable.append(listEl);
+    }
+});
+// We can ask Alan how hard it is to catch every one of them and give it a difficulty rating of 1 - 5. 
